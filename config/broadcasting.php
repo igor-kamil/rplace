679870a70f0b69ccdeed74845418e2ebd166c362
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-10-16 01:30:22 +0200

    separate config for soketi and pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,7 +30,7 @@ return [
 
     'connections' => [
 
-        'pusher' => [
+        'soketi' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY', 'app-key'),
             'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
@@ -41,6 +41,24 @@ return [
                 'scheme' => env('PUSHER_SCHEME', 'http'),
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME') === 'https',
+            ],
+        ],
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'port' => env('PUSHER_PORT', 443),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+            ],
+            'client_options' => [
+                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
 
